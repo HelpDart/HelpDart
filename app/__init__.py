3ba0f7bd972992f08dda92bdc340b43f5184a245
Aarav Shah <107724861+a-shah008@users.noreply.github.com>
2024-07-23 22:50:44 -0500

    Updated init file with environ vars

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,13 @@ def create_app():
     
     app.config["SECRET_KEY"] = "helpdart"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USERNAME'] = os.environ.get("DESIGNATED_EMAIL")
+    app.config['MAIL_PASSWORD'] = os.environ.get("DESIGNATED_EMAIL_PW")
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
     
     db.init_app(app)
 
